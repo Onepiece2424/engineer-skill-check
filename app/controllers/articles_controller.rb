c47ff9afa71ade83c7dcf7bfaa7cc40ac0e61be1
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-05-29 15:07:57 +0900

    index,new,createメソッドに変数とparamsの作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,24 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.create(article_params)
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
