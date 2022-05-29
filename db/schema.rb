d237ddf9d4ff66fc6e0610ee7423618316eb6152
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-05-29 15:02:59 +0900

    Artilceモデルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_29_052435) do
-
-  create_table "artilces", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2022_05_28_101252) do
 
   create_table "departments", force: :cascade do |t|
     t.string "name", null: false
