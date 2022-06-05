dbe969d3166ffd0ef79440a4ff9deeb7227a6c2d
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-06-05 13:26:42 +0900

    AccessDenied エラー発生時にroot_pathへ遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   before_action :user_logged_in?
-  
+
+  rescue_from CanCan::AccessDenied do |_exception|
+    redirect_to root_path, alert: '画面を閲覧する権限がありません。'
+  end
+
   private
     def user_logged_in?
       return if logged_in?
